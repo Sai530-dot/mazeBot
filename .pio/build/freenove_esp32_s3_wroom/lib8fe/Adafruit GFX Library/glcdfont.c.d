1655863e85a89105c74694773e488f81fe3a5330
Ivan <Ivanjuliusoliver@gmail.com>
2026-09-17 15:41:23 -0600

    connected the OLED to the ESP32. Status: working

---
+++ .pio/build/freenove_esp32_s3_wroom/lib8fe/Adafruit GFX Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+.pio/build/freenove_esp32_s3_wroom/lib8fe/Adafruit\ GFX\ Library/glcdfont.c.o: \
+ .pio/libdeps/freenove_esp32_s3_wroom/Adafruit\ GFX\ Library/glcdfont.c
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,54 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-void setup() {
-  Serial.begin(115200);
+#define SDA_PIN 8
+#define SCL_PIN 9
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    &Wire,
+    -1);
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+
+    Serial.println("Starting OLED...");
+
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        Serial.println("OLED initialization FAILED!");
+
+        while (true)
+        {
+        }
+    }
+
+    Serial.println("OLED connected!");
+
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+
+    display.setCursor(20, 15);
+    display.println("DSA MazeBot");
+
+    display.setCursor(30, 35);
+    display.println("OLED OK!");
+
+    display.display();
 }
 
-void loop() {
-  Serial.println("Mazebot alive");
-  delay(1000);
+void loop()
+{
 }
